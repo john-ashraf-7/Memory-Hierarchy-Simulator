66cc96121708950d8e465befc2a34d7f8e04f219
Reem Hamada <70950561+reemhamada2020@users.noreply.github.com>
2024-12-08 00:45:03 +0200

    Update main.py

--- Source Codes/main.py
+++ Source Codes/main.py
@@ -1,16 +1,17 @@
 from inputReader import *
 from cache import *
 
-#read data from user
+# Read data from user
 addrLength, addresses, memorySize, memoryAccessTime, cacheLineSize, cacheSize, cacheAccessTime = readUserData()
-hit_count, miss_count, total_cycles = readFromMemory(addrLength, addresses, memorySize, memoryAccessTime, cacheLineSize, cacheSize, cacheAccessTime);
+hit_count, miss_count, total_cycles = readFromMemory(addrLength, addresses, memorySize, memoryAccessTime, cacheLineSize, cacheSize, cacheAccessTime)
 
-#calculate hit, miss ratios.
-hit_ratio = hit_count/len(addresses)
+# Calculate hit, miss ratios
+hit_ratio = hit_count / len(addresses)
+miss_ratio = 1 - hit_ratio  # Calculate miss_ratio after hit_ratio
 
-#AMAT = Hit time + Miss rate × Miss penalty
-AMAT = (memoryAccessTime) + miss_ratio*(memoryAccessTime + cacheAccessTime); 
-miss_ratio = 1 - hit_ratio
+# AMAT = Hit time + Miss rate × Miss penalty
+AMAT = cacheAccessTime + miss_ratio * (memoryAccessTime + cacheAccessTime)
+
 print("hit_ratio = ", hit_ratio)
 print("miss_ratio = ", miss_ratio)
 print("total number of cycles = ", total_cycles)
