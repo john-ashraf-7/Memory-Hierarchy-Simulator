6384de5254d0e47904a82d477b908ae9ff9e675b
john-ashraf-7 <johnashraf@aucegypt.edu>
2024-12-07 05:20:44 +0200

    converted farid's main.py into inputReader.py and organized further

--- Source Codes/accessSequences.py
+++ Source Codes/accessSequences.py
@@ -5,6 +5,6 @@ def generateAddresses(addrLength):
 		random.seed(i)
 		addrs = [str(random.randint( 0 , 2**int(addrLength/2))) for _ in range(2**int(addrLength/2)*5)]
 		sequence=",".join(addrs)
-		with open(f"sequence{i}.txt","w") as file:
+		with open(f"../Tests/sequence{i}.txt","w") as file:
 			file.write(sequence)
 	return memorySize
---
+++ Source Codes/cache.py
@@ -0,0 +1,10 @@
+
+def hit():
+    print("Hit!")
+def miss():
+    print("Miss!")
+
+cache = {}
+
+def readFromMemory(addrLength, memorySize, memoryAccessTime, cacheLineSize, cacheSize, cacheAccessTime, addrs):
+    pass
